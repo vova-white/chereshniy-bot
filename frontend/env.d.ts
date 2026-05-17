119191abe5cd1a4ce297b17769f1f27a3e180891
Vladimir <belyj.vladimir@gmail.com>
2026-05-17 10:45:16 +0200

    fix: env types

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -2,6 +2,6 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, unknown>
+  const component: DefineComponent<Record<string, never>, Record<string, never>, unknown>
   export default component
 }
